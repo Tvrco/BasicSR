612cdbb345667415a7feae516e9b481d32a188c5
Tvrco <pjh121@163.com>
2023-05-05 14:40:11 +0800

    sh

--- data.sh
+++ data.sh
@@ -4,6 +4,8 @@ unzip -q /content/drive/MyDrive/data/DIV2K/DIV2K_valid_HR.zip -d /content/BasicS
 echo 'unzip DIV2K_valid_HR done'
 unzip -q /content/drive/MyDrive/data/Flickr2K/Flickr2K.zip -d /content/BasicSR/datasets
 echo 'unzip Flickr2K done'
+unzip -q /content/drive/MyDrive/data/Classical/Set5.zip -d /content/BasicSR/datasets
+echo 'unzip Set5 done'
 python /content/drive/MyDrive/data/predata.py -w --scale LRbicx4 --hr_img_dir /content/BasicSR/datasets/DIV2K_train_HR --lr_img_dir /content/BasicSR/datasets/DF2K
 python /content/drive/MyDrive/data/predata.py -w --scale LRbicx4 --hr_img_dir /content/BasicSR/datasets/DIV2K_valid_HR --lr_img_dir /content/BasicSR/datasets/DF2K_val
 python /content/drive/MyDrive/data/predata.py -w --scale LRbicx4 --hr_img_dir /content/BasicSR/datasets/Flickr2K --lr_img_dir /content/BasicSR/datasets/DF2K
